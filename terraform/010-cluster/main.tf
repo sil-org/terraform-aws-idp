303867f7977fae2235470372e44b58f7dcd4c3d9
forevermatt <forevermatt@users.noreply.github.com>
2025-08-13 11:45:10 -0400

    Remove email-service-api/-cron from CloudWatch Dashboard
    We no longer use those, and we plan to remove them. Sending emails was moved into ID Broker.

--- terraform/010-cluster/main.tf
+++ terraform/010-cluster/main.tf
@@ -127,8 +127,6 @@ module "ecs-service-cloudwatch-dashboard" {
   dashboard_name = "${var.app_name}-${var.app_env}-${data.aws_region.current.name}"
 
   service_names = [
-    "${var.idp_name}-email-service-api",
-    "${var.idp_name}-email-service-cron",
     "${var.idp_name}-id-broker",
     "${var.idp_name}-phpmyadmin",
     "${var.idp_name}-pw-manager",
