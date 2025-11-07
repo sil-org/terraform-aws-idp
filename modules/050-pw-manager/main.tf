90335be5c1f6cd0779b372391c09c2b4cda9cff4
briskt <3172830+briskt@users.noreply.github.com>
2025-11-07 11:11:10 +0800

    Revert "change the container definitions to listen on both 80 and 443"
    This reverts commit 2ba7ebf06507e5ee7856f3eba782ca4b31993e84.

--- modules/050-pw-manager/main.tf
+++ modules/050-pw-manager/main.tf
@@ -106,6 +106,7 @@ locals {
     password_rule_maxlength             = var.password_rule_maxlength
     password_rule_minlength             = var.password_rule_minlength
     password_rule_minscore              = var.password_rule_minscore
+    port                                = var.enable_tls ? "443" : "80"
     recaptcha_secret_key                = var.recaptcha_secret
     recaptcha_site_key                  = var.recaptcha_key
     sentry_dsn                          = var.sentry_dsn
